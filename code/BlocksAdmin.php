76b99e71eefd981f56ee38538575b86d54263289
Leo Chen <leo@saltedherring.com>
2016-08-08 15:25:39 -1200

    remove a debug line

--- code/BlocksAdmin.php
+++ code/BlocksAdmin.php
@@ -4,41 +4,52 @@
  *
  * Left-hand-side tab : Admin Blocks
  * */
-	class BlocksAdmin extends ModelAdmin {
-		private static $managed_models = array('Block');
-		private static $url_segment = 'blocks';
-		private static $menu_title = 'Blocks';
-		private static $menu_priority = 10;
-		private static $menu_icon = 'silverstripe-block/images/icon-block.png';
+class BlocksAdmin extends ModelAdmin {
+	private static $managed_models = array('Block');
+	private static $url_segment = 'blocks';
+	private static $menu_title = 'Blocks';
+	private static $menu_priority = 10;
+	private static $menu_icon = 'silverstripe-block/images/icon-block.png';
+		
+	public function getEditForm($id = null, $fields = null) {
+		$form = parent::getEditForm($id, $fields);
+		
+		$grid = $form->Fields()->fieldByName($this->sanitiseClassName($this->modelClass));
+		
+		
+		$grid->getConfig()
+			->removeComponentsByType('GridFieldPaginator')
+			->removeComponentsByType('GridFieldAddNewButton')
+			->removeComponentsByType('GridFieldPrintButton')
+			->removeComponentsByType('GridFieldExportButton')
+			->addComponents(
+				new GridFieldPaginatorWithShowAll(30),
+				$multiClass = new MultiClassSelector(),
+				$sortable = new GridFieldOrderableRows('SortOrder')
+			);
 			
-		public function getEditForm($id = null, $fields = null) {
-			$form = parent::getEditForm($id, $fields);
-			
-			$grid = $form->Fields()->fieldByName($this->sanitiseClassName($this->modelClass));
-			
-			$grid->getConfig()
-				->removeComponentsByType('GridFieldPaginator')
-				->removeComponentsByType('GridFieldAddNewButton')
-				->removeComponentsByType('GridFieldPrintButton')
-				->removeComponentsByType('GridFieldExportButton')
-				->addComponents(
-					new GridFieldPaginatorWithShowAll(30),
-					$multiClass = new MultiClassSelector(),
-					$sortable = new GridFieldOrderableRows('SortOrder')
-				);
-			
-			$subBlocks = ClassInfo::subclassesFor('Block');
-			if (is_null($subBlocks)) {
-				$subBlocks = array('Block');
-			}else{
-				unset($subBlocks['Block']);
-				foreach ($subBlocks as $key => &$value) {
-					$value = empty($key::$singular_name) ? ucwords(trim(strtolower(preg_replace('/_?([A-Z])/', ' $1', $value)))) : $key::$singular_name;
+		$subBlocks = self::getAvaiableTypes();
+		$multiClass->setClasses($subBlocks);
+		$grid->setTitle('All Blcoks');
+		return $form;
+	}
+	
+	public static function getAvaiableTypes() {
+		$subBlocks = ClassInfo::subclassesFor('Block');
+		if (is_null($subBlocks)) {
+			$subBlocks = array('Block');
+		}else{
+			$disabledTypes = Config::inst()->get('Block','DisabledBlocks');
+			if (!empty($disabledTypes)) {
+				foreach ($disabledTypes as $disabledType) {
+					unset($subBlocks[$disabledType]);
 				}
 			}
-			
-			$multiClass->setClasses($subBlocks);
-			$grid->setTitle('All Blcoks');
-			return $form;
+			foreach ($subBlocks as $key => &$value) {
+				$value = empty($key::$singular_name) ? ucwords(trim(strtolower(preg_replace('/_?([A-Z])/', ' $1', $value)))) : $key::$singular_name;
+			}
 		}
+		
+		return $subBlocks;
 	}
+}
